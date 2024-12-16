b3084340e18339fc0fa5031a4eeeab23032fd548
Aum Patel <aumpatel131@gmail.com>
2024-12-16 15:13:31 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './frontend/**/*.html',  // All HTML files in the frontend folder
+    './frontend/**/*.js',    // All JS files in the frontend folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
